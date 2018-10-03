895c3caa49f241751278345b20b26abd2a866c12
goombeer <highbridge71@gmail.com>
2018-10-03 16:41:22 +0900

    bladeの分割

---
+++ resources/views/layouts/common.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+@yield('head')
+</head>
+<body>
+@yield('header')
+<div class="contents">
+    <div class="main">
+        @yield('content')
+    </div>
+</div>
+@yield('footer')
+</body>
+</html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+@section('footer')
+<footer class="footer">
+    フッター
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+@yield('pageJs')
+@endsection
