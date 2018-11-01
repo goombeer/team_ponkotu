b2015186be39c9e360c027954c8808d3dcf725f5
YuukiTakahashi <highbridge71@gmail.com>
2018-11-02 00:34:49 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2018_11_01_152036_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('project_id')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('message_purpose');
+            $table->date('post_deadline');
+            $table->string('reminder_mail_adress')->unique('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
